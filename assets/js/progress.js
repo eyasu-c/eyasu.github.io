450f7ca816ca2d873b3ddfb1b80b5cea79546cff
eyasu-c <115325059+eyasu-c@users.noreply.github.com>
2023-01-24 19:59:37 +0000

    new update

--- assets/js/progress.js
+++ assets/js/progress.js
@@ -1,30 +1,40 @@
-
-window.addEventListener('scroll', function(){
-    var scroll = document.querySelector('.back-to-top');
-    scroll.classList.toggle("back-to-top-active", window.scrollY > 500)
-})
-
-function scrollToTop(){
+window.addEventListener("scroll", function () {
+    var scroll = document.querySelector(".back-to-top");
+    scroll.classList.toggle("back-to-top-active", window.scrollY > 500);
+  });
+  window.addEventListener("scroll", function () {
+    var scroll = document.querySelectorAll(".skill-bars");
+    scroll.forEach((element) => {
+      element.classList.toggle("skill-bar", window.scrollY > 1300);
+    });
+  });
+  
+  function scrollToTop() {
     window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-}
-
-
-var ww = $(window).width();
-var limit = 700;
-
-function refresh() {
-   ww = $(window).width();
-   var w =  ww<limit ? (location.reload(true)) :  ( ww>limit ? (location.reload(true)) : ww=limit );
-}
-
-var tOut;
-$(window).resize(function() {
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+  
+  var ww = $(window).width();
+  var limit = 700;
+  
+  function refresh() {
+    ww = $(window).width();
+    var w =
+      ww < limit
+        ? location.reload(true)
+        : ww > limit
+        ? location.reload(true)
+        : (ww = limit);
+  }
+  
+  var tOut;
+  $(window).resize(function () {
     var resW = $(window).width();
     clearTimeout(tOut);
-    if ( (ww>limit && resW<limit) || (ww<limit && resW>limit) ) {        
-        tOut = setTimeout(refresh, 100);
+    if ((ww > limit && resW < limit) || (ww < limit && resW > limit)) {
+      tOut = setTimeout(refresh, 100);
     }
-});
+  });
+  
